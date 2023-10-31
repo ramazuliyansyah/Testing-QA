d8808256b3bde2d4890c3ad49f001e95c061dc51
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2023-10-31 10:54:12 +0700

    Add files via upload

---
+++ Test_Unit.py
@@ -0,0 +1,21 @@
+# Modul yang akan diuji
+def tambah(a, b):
+    return a + b
+
+# Unit test
+import unittest
+
+class TestTambah(unittest.TestCase):
+
+    def test_tambah_positif(self):
+        self.assertEqual(tambah(2, 3), 5)
+
+    def test_tambah_negatif(self):
+        self.assertEqual(tambah(-2, -3), -5)
+
+    def test_tambah_campuran(self):
+        self.assertEqual(tambah(-2, 3), 1)
+        self.assertEqual(tambah(2, -3), -1)
+
+if __name__ == '__main__':
+    unittest.main()
