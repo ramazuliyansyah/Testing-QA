2a4208ca8cbed8308c4f7779c35cc8f0b2d54119
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2023-11-01 23:07:55 +0700

    Update Test_Unit.py

--- Test_Unit.py
+++ Test_Unit.py
@@ -8,7 +8,7 @@ import unittest
 class TestTambah(unittest.TestCase):
 
     def test_tambah_positif(self):
-        self.assertEqual(tambah(2, 3), 10)
+        self.assertEqual(tambah(2, 3), 5)
 
     def test_tambah_negatif(self):
         self.assertEqual(tambah(-2, -3), -5)
