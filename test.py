c46feec0a2a89864b80ce97bb0659731455ced83
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2023-11-01 23:21:45 +0700

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestTambah(unittest.TestCase):
 
     # Fungsi untuk menguji apakah fungsi tambah berperilaku sesuai dengan harapan
     def test_tambah(self):
-        self.assertEqual(tambah(2, 3), 10)  # Memastikan 2 + 3 = 5
+        self.assertEqual(tambah(2, 3), 5)  # Memastikan 2 + 3 = 5
         self.assertEqual(tambah(0, 0), 0)  # Memastikan 0 + 0 = 0
         self.assertEqual(tambah(-1, 1), 0)  # Memastikan -1 + 1 = 0
         self.assertEqual(tambah(-1, -1), -2)  # Memastikan -1 + (-1) = -2
