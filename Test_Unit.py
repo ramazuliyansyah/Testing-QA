976ca352fefdd32d114ab42f24cf8dc8dd84aa30
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2023-11-02 11:39:04 +0700

    Update Test_Unit.py

--- Test_Unit.py
+++ Test_Unit.py
@@ -8,14 +8,14 @@ import unittest
 class TestTambah(unittest.TestCase):
 
     def test_tambah_positif(self):
-        self.assertEqual(tambah(2, 3), 5)
+        self.assertEqual(tambah(2, 3), 5) #Memastikan penjumlahan bilangan positif 2 + 3 = 5 benar
 
     def test_tambah_negatif(self):
-        self.assertEqual(tambah(-2, -3), -5)
+        self.assertEqual(tambah(-2, -3), -5) #Memastikan penjumlahan bilangan negatif -2 + -3 = 5 benar
 
     def test_tambah_campuran(self):
-        self.assertEqual(tambah(-2, 3), 1)
-        self.assertEqual(tambah(2, -3), -1)
+        self.assertEqual(tambah(-2, 3), 1) #Memastikan penjumlahan bilangan campuran benar -2 + 3 = 1 benar
+        self.assertEqual(tambah(2, -3), -1) #Memastikan penjumlahan bilangan campuran benar 2 + -3 = -1 benar
 
 if __name__ == '__main__':
     unittest.main()
